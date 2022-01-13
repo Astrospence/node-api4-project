439c04a72b868ffb4a6651c7fd9ebb2f7625d8ca
Astrospence <illustrator.wood@gmail.com>
2022-01-13 14:17:08 -0700

    wrote post endpoint

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+const jsonParser = bodyParser.json()
 
 const fakeUsers = [
     {
-        id: 1,
         name: 'Charles',
-        hobby: 'LEGO'
+        hobby: 'LEGO',
+        username: 'charles',
+        password: 'legoRocks'
     },
     {
-        id: 2,
         name: 'Victoria',
-        hobby: 'Explosives'
+        hobby: 'Explosives',
+        username: 'victoria',
+        password: 'boomGoesTheDynamite'
     }
 ]
 
 app.get('/api/users', (req, res) => {
     res.status(200).json(fakeUsers)
+})
+
+app.post('/api/register', jsonParser, (req, res) => {
+    const newUser = {
+        name: req.body.name,
+        hobby: req.body.hobby,
+        username: req.body.username,
+        password: req.body.password
+    }
+    res.status(200).json(newUser)
 })
 
 const port = process.env.PORT || 9000
