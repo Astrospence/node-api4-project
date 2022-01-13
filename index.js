4f77496831ba39ae9cd3c2fac1abb48c3d1278a9
Astrospence <illustrator.wood@gmail.com>
2022-01-13 13:37:18 -0700

    wrote get endpoint

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const fakeUsers = [
+    {
+        id: 1,
+        name: 'Charles',
+        hobby: 'LEGO'
+    },
+    {
+        id: 2,
+        name: 'Victoria',
+        hobby: 'Explosives'
+    }
+]
 
+app.get('/api/users', (req, res) => {
+    res.status(200).json(fakeUsers)
+})
 
 const port = process.env.PORT || 9000
 
