987f5f993d040813f79423b6b7d13151e3102bb1
Astrospence <illustrator.wood@gmail.com>
2022-01-13 13:24:29 -0700

    created index.js and initialized server

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+
+
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
