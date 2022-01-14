832ddeca5eb353331b30286793f2059abbe4e8b4
Astrospence <illustrator.wood@gmail.com>
2022-01-13 21:52:32 -0700

    wrote second post endpoint

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ app.post('/api/register', jsonParser, (req, res) => {
     res.status(200).json(newUser)
 })
 
+app.post('/api/login', jsonParser, (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(403).json({ message: 'Invalid username or password'})
+    } else {
+        res.status(200).json({ message: 'Welcome User!'})
+    }
+})
+
 const port = process.env.PORT || 9000
 
 app.listen(port, () => {
